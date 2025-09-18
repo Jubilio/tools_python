9d7b2ca188d663cf086d659227b85556b8e11b7f
Jubilio <jubiliomausse5@gmail.com>
2025-09-18 08:55:50 -0400

    feat: config yml, and new feature.:

--- achilles/achilles.py
+++ achilles/achilles.py
@@ -13,13 +13,18 @@ parser = argparse.ArgumentParser(description="The Achilles HTML Vulnerabitility 
 
 parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
 parser.add_argument('url', type=str, help='The target URL of HTML to analyze')
-parser.add_argument('--config', type=str, help='Path to configuration file', default=None)
+parser.add_argument('--config', help='Path to configuration file')
+parser.add_argument('-o', '--output', help='Report file output path')
 
 args = parser.parse_args()
 
+config = {'forms': True, 'comments': True, 'passwords': True}
 if(args.config):
     print('Using config file: ' + args.config)
     config_file = open(args.config, 'r')
+    config_from_file = yaml.load(config_file, Loader=yaml.FullLoader)
+    if(config_from_file):
+        config = { **config, **config_from_file }
     
 
 report = ''
@@ -33,26 +38,39 @@ if (validators.url(url)):
     forms           = parsed_html.find_all('form')
     comments        = parsed_html.find_all(string=lambda text: isinstance(text, Comment))
     password_inputs = parsed_html.find_all('input', {'name': 'password'})
-    
-    for form in forms:
-        if((form.get('action').find('https') < 0 and (urlparse(url).scheme != 'https'))):
-            report += '[!] Form with non-HTTPS action found: Insecure form action ' + form.get('action') + ' found in document \n{}\n'.format(form)
 
-    for comment in comments:
-        if(comment.find('key: ') > -1):
-            report += '[!] Sensitive information found in HTML comments: ' + comment + '\n'
+    if(config['forms']):
+        for form in forms:
+            if((form.get('action').find('https') < 0 and (urlparse(url).scheme != 'https'))):
+                report += '[!] Form with non-HTTPS action found: Insecure form action ' + form.get('action') + ' found in document \n{}\n'.format(form)
 
-    for password_input in password_inputs:
-        if(password_input.get('type') != 'password'):
-            report += '[!] Insecure password input field found: ' + str(password_input) + '\n'
+    if(config['comments']):
+        for comment in comments:
+            if(comment.find('key: ') > -1):
+                report += '[!] Sensitive information found in HTML comments: ' + comment + '\n'
+
+    if(config['passwords']):
+        for password_input in password_inputs:
+            if(password_input.get('type') != 'password'):
+                report += '[!] Insecure password input field found: ' + str(password_input) + '\n'
     
 else:
     print('Invalid URL. Please provide a valid URL scheme.')
 
 
 if(report == ''):
-    print('No issues found.')
+    print('No issues found. Your HTML documents is secure!\n')
 else:  
-    print("Vulnerability Report is as follows:\n") 
-    print('-----------------------------------\n')
-    print(report)
+    header  = '-----------------------------------\n'
+    header += "Vulnerability Report is as follows:\n" 
+    header += '-----------------------------------\n\n'
+    
+    report = header + report
+
+print(report)
+
+if(args.output):
+    f = open(args.output, 'w')
+    f.write(report)
+    f.close
+    print('Report saved at: ' + args.output)
